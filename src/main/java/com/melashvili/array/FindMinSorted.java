a817901c4da43b2c7a4fc6b1c1c452fd168e7e83
NikaMelashvili <melashvilinikusha63@gmail.com>
2025-03-06 13:28:18 +0400

    Sorted array log n algorithm

--- src/main/java/com/melashvili/array/FindMinSorted.java
+++ src/main/java/com/melashvili/array/FindMinSorted.java
@@ -9,15 +9,37 @@ public class FindMinSorted {
 
     public static void main( String[] args )
     {
-        System.out.println(findMin(nums1));
-        System.out.println(findMin(nums2));
-        System.out.println(findMin(nums3));
+        System.out.println(findMinON(nums1));
+        System.out.println(findMinON(nums2));
+        System.out.println(findMinON(nums3));
+
+        System.out.println("--------------------------");
+
+        System.out.println(findMinLogN(nums1));
+        System.out.println(findMinLogN(nums2));
+        System.out.println(findMinLogN(nums3));
     }
 
-    public static int findMin(int[] nums) {
+    public static int findMinON(int[] nums) {
         for(int i = 0; i < nums.length - 1; i++) {
             if(nums[i + 1] < nums[i]) return nums[i + 1];
         }
         return nums[0];
+    }
+
+    public static int findMinLogN(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while(left < right) {
+            int mid = left + (right - left) / 2;
+
+            if  (nums[mid] <= nums[right]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return nums[left];
     }
 }
